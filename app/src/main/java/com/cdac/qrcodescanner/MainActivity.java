992ce91021b11334f927174a7cb4e637dc95e327
tarukhan19@gmail.com <communication@19>
2020-12-21 16:41:00 +0530

    21dec

---
+++ app/src/main/java/com/cdac/qrcodescanner/MainActivity.java
@@ -0,0 +1,34 @@
+package com.cdac.qrcodescanner;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initComponents();
+    }
+
+    private void initComponents(){
+        findViewById(R.id.buttonTakePicture).setOnClickListener(this);
+        findViewById(R.id.buttonScanBarcode).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.buttonScanBarcode:
+                startActivity(new Intent(this,ScannerBarcodeActivity.class));
+                break;
+            case R.id.buttonTakePicture:
+                startActivity(new Intent(this,PictureBarcodeActivity.class));
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/cdac/qrcodescanner/ScannerBarcodeActivity.java
@@ -0,0 +1,210 @@
+package com.cdac.qrcodescanner;
+
+import android.Manifest;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.cdac.qrcodescanner.ui.login.LoginActivity;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+public class ScannerBarcodeActivity extends AppCompatActivity {
+
+    SurfaceView surfaceView;
+    TextView textViewBarCodeValue;
+    private BarcodeDetector barcodeDetector;
+    private CameraSource cameraSource;
+    private static final int REQUEST_CAMERA_PERMISSION = 201;
+    String intentData = "";
+    private RequestQueue requestQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanner_barcode);
+        initComponents();
+    }
+
+    private void initComponents() {
+        textViewBarCodeValue = findViewById(R.id.txtBarcodeValue);
+        surfaceView = findViewById(R.id.surfaceView);
+    }
+
+    private void initialiseDetectorsAndSources() {
+        Toast.makeText(getApplicationContext(), "Barcode scanner started", Toast.LENGTH_SHORT).show();
+        barcodeDetector = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.ALL_FORMATS)
+                .build();
+
+        cameraSource = new CameraSource.Builder(this, barcodeDetector)
+                .setRequestedPreviewSize(1920, 1080)
+                .setAutoFocusEnabled(true) //you should add this feature
+                .build();
+
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                openCamera();
+            }
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            }
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+                Toast.makeText(getApplicationContext(), "To prevent memory leaks barcode scanner has been stopped", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> barCode = detections.getDetectedItems();
+                if (barCode.size() > 0) {
+                    setBarCode(barCode);
+                }
+            }
+        });
+    }
+
+    private void openCamera(){
+        try {
+            if (ActivityCompat.checkSelfPermission(ScannerBarcodeActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                cameraSource.start(surfaceView.getHolder());
+            } else {
+                ActivityCompat.requestPermissions(ScannerBarcodeActivity.this, new
+                        String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setBarCode(final SparseArray<Barcode> barCode)
+    {
+        textViewBarCodeValue.post(new Runnable() {
+            @Override
+            public void run()
+            {
+                loadProducts(barCode);
+
+
+            }
+        });
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        cameraSource.release();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initialiseDetectorsAndSources();
+    }
+
+    private void copyToClipBoard(String text){
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("QR code Scanner", text);
+        clipboard.setPrimaryClip(clip);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == REQUEST_CAMERA_PERMISSION && grantResults.length>0){
+            if (grantResults[0] == PackageManager.PERMISSION_DENIED)
+                finish();
+            else
+                openCamera();
+        }else
+            finish();
+    }
+
+
+    private void loadProducts(final SparseArray<Barcode> barCode) {
+
+        requestQueue = Volley.newRequestQueue(this);
+
+        StringRequest postRequest = new StringRequest(Request.Method.POST, "https://gstsuvidhacenters.in/BRY_Aushadhi_Kendra_Products.ashx",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.e("respone",response);
+                        try {
+                            JSONObject obj = new JSONObject(response);
+                            int id = obj.optInt("Id");
+                            String msg = obj.optString("Status");
+                            if (id == 1 && msg.equalsIgnoreCase("Success")) {
+                                intentData = barCode.valueAt(0).displayValue;
+                                textViewBarCodeValue.setText(intentData);
+                                copyToClipBoard(intentData);
+                                startActivity(new Intent(ScannerBarcodeActivity.this, LoginActivity.class));
+
+                            }
+                            else
+                            {
+
+                            }
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                }
+        ) {
+
+        };
+        int socketTimeout = 30000;
+        RetryPolicy policy = new DefaultRetryPolicy(socketTimeout, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+        postRequest.setRetryPolicy(policy);
+        requestQueue.add(postRequest);
+    }
+
+}
